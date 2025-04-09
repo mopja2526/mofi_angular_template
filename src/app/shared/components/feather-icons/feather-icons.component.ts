17a01c50f438e88a36af6d3daeaddaa942df54d4
KTBDOMAIN\671026 <sakunvatw@gmail.com>
2025-04-09 10:47:59 +0700

    add all

---
+++ src/app/shared/components/feather-icons/feather-icons.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import * as feather from 'feather-icons';
+
+@Component({
+  selector: 'app-feather-icons',
+  templateUrl: './feather-icons.component.html',
+  styleUrls: ['./feather-icons.component.scss']
+})
+
+export class FeatherIconsComponent {
+
+  @Input() icons: string | any;
+  @Input() class: string | any;
+
+ 
+  ngAfterViewInit() {
+    feather.replace();
+  }
+}
---
+++ src/app/shared/routes/routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from "@angular/router";
+import { SamplePageComponent } from "src/app/components/simple-page/sample-page/sample-page.component";
+
+export const content: Routes = [
+    {
+        path: 'sample-page',
+        component :SamplePageComponent,
+        data: {
+            title: "Sample Page",
+            breadcrumb: "Sample Page",
+          },
+    },
+    {
+        path: 'pages',
+        loadChildren: () => import('../../components/simple-page/simple-page.module').then(m => m.SimplePageModule),
+        data: {
+            title: "Sample Page",
+            breadcrumb: "Sample Page",
+        }
+    },
+]
+
+
